dbba4fe613aeea7afb8b904ad512d1f4d6fb16da
Manuel Stoeckel <s3676959@stud.uni-frankfurt.de>
2019-06-13 16:07:16 +0200

    Some fixes in TextAnnotatorFetch, to support XMIs with missing type systems and existing DocumentMetaData.

--- textimager-uima-biofid-ocr-parser/src/main/java/BIOfid/Extraction/TextAnnotatorFetch.java
+++ textimager-uima-biofid-ocr-parser/src/main/java/BIOfid/Extraction/TextAnnotatorFetch.java
@@ -1,16 +1,17 @@
 package BIOfid.Extraction;
 
 import BIOfid.AbstractRunner;
-import com.google.common.io.Files;
+import BIOfid.OCR.Annotation.Document;
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.client.HttpResponseException;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.cas.impl.XmiCasSerializer;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.JCasFactory;
-import org.apache.uima.fit.util.CasIOUtil;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.util.CasCopier;
 import org.apache.uima.util.CasIOUtils;
@@ -18,9 +19,12 @@ import org.hucompute.utilities.helper.RESTUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
@@ -29,20 +33,20 @@ import java.util.stream.IntStream;
 import static BIOfid.Utility.Util.writeToFile;
 
 public class TextAnnotatorFetch extends AbstractRunner {
-
+	
 	private static final String textannotator = "http://141.2.108.253:50555/";
 	private static final String mongoDB = "https://resources.hucompute.org/mongo/";
-
+	
 	private static String sRepository = "14393";
 	private static String sSession;
 	private static String conllLocation;
 	private static String XMILocation;
 	private static String textLocation;
-
+	
 	public static void main(String[] args) {
 		try {
 			getParams(args);
-
+			
 			int index;
 			index = Integer.max(params.indexOf("-s"), params.indexOf("--session"));
 			if (index > -1) {
@@ -50,116 +54,129 @@ public class TextAnnotatorFetch extends AbstractRunner {
 			} else {
 				throw new MissingArgumentException("Missing --session!\n");
 			}
-
+			
 			index = params.indexOf("--xmi");
 			if (index > -1) {
 				XMILocation = params.get(index + 1);
 			} else {
 				throw new MissingArgumentException("Missing --xmi!\n");
 			}
-
+			
 			index = params.indexOf("--conll");
 			if (index > -1) {
 				conllLocation = params.get(index + 1);
 			} else {
 				throw new MissingArgumentException("Missing --conll!\n");
 			}
-
+			
 			index = params.indexOf("--text");
 			if (index > -1) {
 				textLocation = params.get(index + 1);
 			} else {
 				throw new MissingArgumentException("Missing --text!\n");
 			}
-
+			
 			index = params.indexOf("--threads");
 			final int pThreads = index > -1 ? Integer.parseInt(params.get(index + 1)) : 4;
-
+			
 			final boolean forceUTF8 = params.indexOf("--forceUTF8") > -1;
 			final boolean reserialize = forceUTF8 & params.indexOf("--reserialize") > -1;
-
+			
 			index = params.indexOf("--strategyIndex");
 			final int strategyIndex = (index > -1) ? Integer.parseInt(params.get(index)) : 0;
-
+			
 			index = params.indexOf("--repository");
 			if (index > -1) {
 				sRepository = params.get(index + 1);
 			}
-
+			
 			String requestURL = textannotator + "documents/" + sRepository;
 			final JSONObject remoteFiles = RESTUtils.getObjectFromRest(requestURL, sSession);
-
+			
 			if (remoteFiles.getBoolean("success")) {
 				final JSONArray rArray = remoteFiles.getJSONArray("result");
-
+				
 				try {
 					final ForkJoinPool remotePool = new ForkJoinPool(pThreads);
-
+					
 					System.out.printf("Running TextAnnotatorFetch in parallel with %d threads for %d files\n", remotePool.getParallelism(), rArray.length());
-
+					
 					final AnalysisEngine conllEngine = AnalysisEngineFactory.createEngine(
 							ConllBIO2003Writer.class,
 							ConllBIO2003Writer.PARAM_TARGET_LOCATION, conllLocation,
 							ConllBIO2003Writer.PARAM_STRATEGY_INDEX, strategyIndex,
 							ConllBIO2003Writer.PARAM_OVERWRITE, true,
 							ConllBIO2003Writer.PARAM_FILTER_FINGERPRINTED, true);
-
+					
 					int[] count = {0};
 					remotePool.submit(() -> IntStream.range(0, rArray.length()).parallel().forEach(a -> {
 						try {
 							String documentURI = mongoDB + rArray.get(a).toString();
 							JSONObject documentJSON = RESTUtils.getObjectFromRest(documentURI, sSession);
-
+							
 							if (documentJSON.getBoolean("success")) {
 								String documentName = documentJSON.getJSONObject("result").getString("name");
-								String cleanDocumentName = documentName.replaceFirst("[^_]*_(\\d+)_.*", "$1");
-
+								String cleanDocumentName = documentName
+										.replaceFirst("[^_]*_(\\d+)_.*", "$1")
+										.replaceAll("\\.[^.]+$", "");
+								
 								// Download XMI
 								URL casURL = new URL(textannotator + "cas/" + rArray.get(a).toString() + "?session=" + sSession);
-
+								
 								// Process XMI & write conll
 								JCas jCas = JCasFactory.createJCas();
-
-								File utf8File = Paths.get(XMILocation, cleanDocumentName + ".xmi").toFile();
+								
+								Path utf8Path = Paths.get(XMILocation, cleanDocumentName + ".xmi");
+								File utf8File = utf8Path.toFile();
 								if (forceUTF8) {
-									PrintWriter printWriter = new PrintWriter(Files.newWriter(utf8File, StandardCharsets.UTF_8));
-									BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(casURL.openStream()));
-									bufferedReader.lines().forEachOrdered(printWriter::println);
-									bufferedReader.close();
-									printWriter.close();
-									CasIOUtil.readXmi(jCas, utf8File);
+									try {
+										FileUtils.copyInputStreamToFile(casURL.openStream(), utf8File);
+									} catch (IOException ioE) {
+										System.err.println("Could not write UTF-8 file!");
+										ioE.printStackTrace();
+									}
+									try (FileInputStream inputStream = FileUtils.openInputStream(utf8File)) {
+										CasIOUtils.load(inputStream, null, jCas.getCas(), true);
+									} catch (IOException ioE) {
+										System.err.println("Could not read UTF-8 file!");
+										ioE.printStackTrace();
+									}
 								} else {
 									CasIOUtils.load(casURL, jCas.getCas());
 								}
-
-								DocumentMetaData documentMetaData = DocumentMetaData.create(jCas);
-								documentMetaData.setDocumentId(cleanDocumentName);
-								documentMetaData.setDocumentUri(documentURI);
-								documentMetaData.setDocumentTitle(documentName);
-
+								
+								if (JCasUtil.select(jCas, DocumentMetaData.class).size() == 0) {
+									DocumentMetaData documentMetaData = DocumentMetaData.create(jCas);
+									documentMetaData.setDocumentId(cleanDocumentName);
+									documentMetaData.setDocumentUri(documentURI);
+									documentMetaData.setDocumentTitle(documentName);
+								}
+								
 								try {
 									JCas lCas = JCasFactory.createJCas();
 									CasCopier.copyCas(jCas.getCas(), lCas.getCas(), true);
-
-									DocumentMetaData ldocumentMetaData = DocumentMetaData.create(lCas);
-									ldocumentMetaData.setDocumentId(cleanDocumentName);
-									ldocumentMetaData.setDocumentUri(documentURI);
-									ldocumentMetaData.setDocumentTitle(documentName);
-
+									
+									DocumentMetaData.copy(jCas, lCas);
+									
 									conllEngine.process(lCas);
 								} catch (UIMAException e) {
 									e.printStackTrace();
 								}
-
+								
 								// Reserialize the UTF-8 forced JCas
 								if (!forceUTF8 || reserialize) {
-									XmiCasSerializer.serialize(jCas.getCas(), new FileOutputStream(utf8File));
+									try (FileOutputStream fs = new FileOutputStream(utf8File)) {
+										XmiCasSerializer.serialize(jCas.getCas(), fs);
+									} catch (IOException ioE) {
+										System.err.println("Could not write UTF-8 file!");
+										ioE.printStackTrace();
+									}
 								}
-
+								
 								// Write raw text
 								if (jCas.getDocumentText() == null || jCas.getDocumentText().isEmpty())
 									return;
-
+								
 								String content = jCas.getDocumentText();
 								writeToFile(Paths.get(textLocation, cleanDocumentName + ".txt"), content);
 							} else {
@@ -167,16 +184,13 @@ public class TextAnnotatorFetch extends AbstractRunner {
 							}
 						} catch (HttpResponseException httpE) {
 							System.err.println(httpE.getMessage());
-						} catch (IOException ioE) {
-							System.err.println("Could not write UTF-8 file!");
-							ioE.printStackTrace();
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
 						System.out.printf("\rFile %d/%d (running remote threads: %d, remote pool size: %d)",
 								count[0]++, rArray.length(), remotePool.getRunningThreadCount(), remotePool.getPoolSize());
 					})).get();
-
+					
 					remotePool.shutdown();
 				} catch (UIMAException e) {
 					e.printStackTrace();
@@ -193,7 +207,7 @@ public class TextAnnotatorFetch extends AbstractRunner {
 			printUsage();
 		}
 	}
-
+	
 	private static void printUsage() {
 		System.out.println("Downloads and processes the current annotations from a Textannotator repository (14393 by default).\n" +
 				"Usage: java -cp $classpaths TextAnnotatorFetch [args]\n" +
